37b7d1a054c2c0395cba9968394d6683637e6e6e
danielyawar <142349030+danielyawar@users.noreply.github.com>
2023-10-29 20:58:08 -0700

    Add files via upload

---
+++ PhraseSolver.java
@@ -0,0 +1,43 @@
+/*
+ * Activity 2.5.2
+ *
+ *  The PhraseSolver class the PhraseSolverGame
+ */
+import java.util.Scanner;
+
+public class PhraseSolver
+{
+  /* your code here - attributes */
+
+  /* your code here - constructor(s) */ 
+  public PhraseSolver()
+  {
+    Player p1= new Player();
+    Player p2= new Player();
+    Board board=new Board();
+  }
+  /* your code here - accessor(s) */
+  
+  /* your code here - mutator(s)  */
+
+  public void play()
+  {
+    boolean solved = false;
+    int currentPlayer = 1;
+
+    Scanner input = new Scanner(System.in);
+    
+    boolean correct = true;
+    while (!solved) 
+    {
+      
+      /* your code here - game logic */
+      
+      
+      /* your code here - determine how game ends */
+      solved = true; 
+    } 
+   
+  }
+  
+}
---
+++ Player.java
@@ -0,0 +1,29 @@
+/*
+ * Activity 2.5.2
+ * 
+ * A Player class the PhraseSolverGame
+ */
+import java.util.Scanner;
+
+public class Player
+{
+  /* your code here - attributes */
+
+  /* your code here - constructor(s) */ 
+  public Player(){
+    int points= 0;
+    System.out.println("enter player name:");
+    Scanner sc= new Scanner(System.in);
+    String newName= sc.nextLine();
+    String name=newName;
+    System.out.println("Hello and welcome to our game, "+ name);
+  }
+  public Player(String inputName){
+    int points= 0;
+    String name=inputName;
+    System.out.println("Hello and welcome to our game, "+ name);
+  }
+  /* your code here - accessor(s) */ 
+  
+  /* your code here - mutator(s) */ 
+}
